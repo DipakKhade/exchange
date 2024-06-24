ad94cb75fd66ee4ae7291e85788e655189709d35
= <=>
2024-06-24 08:28:17 +0530

    header added

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,33 @@
-
+"use client";
+import { usePathname , useRouter } from "next/navigation"
 
 export default function Header(){
+    const path =usePathname()
+    const router=useRouter()
+    console.log(path)
+ return <>
+ 
+ <div className="flex h-14 w-full flex-col justify-center pl-[21px] pr-4">
+        <div className="flex flex-row justify-between">
+        <ul className="flex items-center flex-row p-12">
+            <li className={`ml-[20px] mr-[20px] flex flex-row items-center justify-center ${path.startsWith('/markets') ? 'text-white' : 'text-slate-500'}`}
 
-    return<>
-    <nav>
+             onClick={()=>router.push('/markets')}>Markets</li>
+
+            <li className={`ml-[20px] mr-[20px] flex flex-row items-center justify-center ${path.startsWith('/treads') ? 'text-white' : 'text-slate-500'} `} onClick={()=>router.push('/treads')}>Treads</li>
+
+            {/* <li className="ml-[20px] mr-[20px] flex flex-row items-center justify-center">More</li> */}
+
+        </ul>
+            <div className="flex flex-row">
+
+                <button className="my-auto ml-10 text-nowrap rounded-lg text-[#00c278] px-3 py-1.5 text-sm font-semibold text-greenText hover:opacity-90 bg-[rgb(12,44,36)]">Sign up</button>
+
+                <button className="my-auto ml-10 text-nowrap rounded-lg text-[rgb(76,148,255)] px-3 py-1.5 text-sm font-semibold text-greenText hover:opacity-90 bg-[#18253A]">Sign in</button>
+                
+            </div>
+        </div>
         
-    </nav>
-    </>
+    </div>
+ </>
 }
---
+++ src/app/components/MarketStatusBar.tsx
@@ -0,0 +1,13 @@
+
+
+export default function MarketStatusBar(){
+    return <>
+    <div className="flex items-center flex-row relative w-full overflow-hidden">
+        <div className="flex items-center justify-between flex-row no-scrollbar overflow-auto pr-4">
+            <div className="flex h-[60px] shrink-0 space-x-4">
+                <button>lkjsadklj</button>
+            </div>
+        </div>
+    </div>
+    </>
+}
